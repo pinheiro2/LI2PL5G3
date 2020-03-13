a458ddc262fa322b2d1e84fd3420f4957d80f41b
Beko <carlos.beko.silva@gmail.com>
2020-03-13 15:43:13 +0000

    funcao jogar

--- projeto/Logica.c
+++ projeto/Logica.c
@@ -1,4 +1,9 @@
-//
-// Created by tiago on 12/03/20.
-//
+#include "Dados.h"
+#include <stdio.h>
+#include "Logica.h"
 
+ int jogar(ESTADO *e, COORDENADA c) {
+    printf("jogar %d %d\n", c.coluna, c.linha);
+    e->tab [(c.linha)-1] [(c.coluna)-1] = BRANCA;
+    return 1;
+}
