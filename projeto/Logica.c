d377596a96e28fe3e2a9961c595f193f5130b522
Tiago Pinheiro da Silva <62107479+myagi-ryota@users.noreply.github.com>
2020-03-19 17:21:05 +0000

    check_tabuleiro

--- projeto/Logica.c
+++ projeto/Logica.c
@@ -2,10 +2,30 @@
 #include <stdio.h>
 #include "Logica.h"
 
+ int check_tabuleiro (COORDENADA c) {
+    if (c.linha >= 1 && c.linha <= 8 && c.coluna >= 1 && c.coluna <= 8)
+        return 1;
+    else
+        return 0;
+}
+
  int jogar(ESTADO *e, COORDENADA c) {
     printf("jogar %d %d\n", c.coluna, c.linha);
     set_casa (e, e -> ultima_jogada, PRETA);
     set_casa (e, c, BRANCA);
     e-> ultima_jogada = c;
+
+
+    if (e -> jogador_atual == 1) {
+        e -> jogador_atual = 2;
+        e -> jogadas [e -> num_jogadas].jogador1 = c;
+
+    }
+    else {
+        e -> jogador_atual = 1;
+        e -> jogadas [e -> num_jogadas].jogador2 = c;
+        e -> num_jogadas++;
+    }
+
     return 1;
 }
