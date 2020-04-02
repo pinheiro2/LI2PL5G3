264588b9882ce1a545983eae4e208f8bffcc012e
Tiago Alexandre Ferreira Silva <62107588+LexerPlay@users.noreply.github.com>
2020-04-02 14:25:29 +0100

    Add files via upload

--- projeto/Dados.h
+++ projeto/Dados.h
@@ -1,7 +1,9 @@
 #ifndef PROJETO_DADOS_H
 #define PROJETO_DADOS_H
 
-
+/**
+\brief Tipos de CASA
+*/
 typedef enum {VAZIO = '.',
               BRANCA = '*',
               PRETA = '#',
@@ -9,22 +11,30 @@ typedef enum {VAZIO = '.',
               DOIS = '2'
         } CASA;
 
+/**
+\brief Coordenadas do tabuleiro
+*/
 typedef struct {
     int coluna;
     int linha;
 } COORDENADA;
 
+/**
+\brief Conjunto das jogadas de cada jogador na ronda
+*/
 typedef struct {
     COORDENADA jogador1;
     COORDENADA jogador2;
 } JOGADA;
 
+/**
+\brief Conjunto de todas as JOGADA realizadas
+*/
 typedef JOGADA JOGADAS[32];
 
 /**
 \brief Tipo de dados para o estado
 */
-
 typedef struct {
     /** O tabuleiro */
     CASA tab[8][8];
@@ -64,12 +74,53 @@ void set_casa(ESTADO *e, COORDENADA c, CASA V);
 */
 CASA get_casa(ESTADO *e, COORDENADA c);
 
+/**
+\brief Troca o jogador atual
+@param e Apontador para o estado
+@param c A coordenada
+*/
 void troca_jogador (ESTADO *e);
+
+/**
+\brief Devolve o jogador atual
+@param e Apontador para o estado
+@returns O jogador atual
+*/
 int get_jogador (ESTADO *e);
+
+/**
+\brief Devolve numero de jogadas
+@param e Apontador para o estado
+@returns O numero de jogadas
+*/
 int get_num_jogadas (ESTADO *e);
+
+/**
+\brief Devolve o numero de comandos
+@param e Apontador para o estado
+@returns O numero de comandos
+*/
 int get_num_comando (ESTADO *e);
+
+/**
+\brief Tranforma uma string num array de int
+@param s A string
+@returns Coordenada
+*/
 COORDENADA str_to_coord(char *s);
+
+/**
+\brief
+@param e Apontador para o estado
+@param j Jogada
+@param num_jog Numero de jogadas
+*/
 void armazenar_jogada(ESTADO *e, JOGADA j, int num_jog);
+
+/**
+\brief Cria o estado inicial do jogo
+ @returns Estado inicial do jogo
+*/
 ESTADO *inicializar_estado();
 
 #endif //PROJETO_DADOS_H
--- projeto/Interface.h
+++ projeto/Interface.h
@@ -6,6 +6,7 @@
 void mostrar_tabuleiro(ESTADO *estado);
 void gr_ficheiro(ESTADO *e, char *nome);
 void ler_ficheiro(ESTADO *e, char *nome);
+void prompt(ESTADO *e);
 void mostrar_movs (ESTADO *e);
 int interpretador (ESTADO *e,int *quit);
 #endif //PROJETO_INTERFACE_H
