4e22e460b5d372b1c55b822774be1a1809c34ee0
Carlos Alberto Fernandes Dias da Silva <62109586+bekogod@users.noreply.github.com>
2020-04-02 14:50:47 +0100

    doc interface.h e logica.h (parte1)

--- projeto/Interface.h
+++ projeto/Interface.h
@@ -3,10 +3,38 @@
 #define PROJETO_INTERFACE_H
 #include "Dados.h"
 
+/**
+\brief Desenha o tabuleiro consoante o ESTADO
+@param c Apontador para o estado
+*/
 void mostrar_tabuleiro(ESTADO *estado);
+
+/**
+\brief Grava o estado atual do jogo
+@param c Apontador para o estado
+@param nome a String
+*/
 void gr_ficheiro(ESTADO *e, char *nome);
+
+/**
+\brief Mostra o tabuleiro guardado
+@param c Apontador para o estado
+@param nome a String
+*/
 void ler_ficheiro(ESTADO *e, char *nome);
+
+/**
+\brief Mostra o número de comandos, o jogador atual e número da jogada
+@param c Apontador para o estado
+*/
 void prompt(ESTADO *e);
+
+/**
+\brief Mostra o todas as jogadas realizadas até ao momento
+@param c Apontador para o estado
+*/
 void mostrar_movs (ESTADO *e);
+
+
 int interpretador (ESTADO *e,int *quit);
 #endif //PROJETO_INTERFACE_H
--- projeto/Logica.h
+++ projeto/Logica.h
@@ -2,10 +2,47 @@
 #ifndef PROJETO_LOGICA_H
 #define PROJETO_LOGICA_H
 
+/**
+\brief Dá-nos a informação se a coordenada jogada existe no tabuleiro
+@param c A coordenada
+@returns 1 se a jogada for possível; 0 senão
+int check_tabuleiro (COORDENADA c)
+*/
 int check_tabuleiro (COORDENADA c);
+
+/**
+\brief Dá-nos a informação se a coordenada jogada esta a "1" de distância da posição atual
+@param c Apontador para o estado
+@param c A coordenada
+@returns 1 se a jogada for possível; 0 senão
+int check_proximidade(ESTADO *e, COORDENADA c)
+*/
 int check_proximidade (ESTADO *e, COORDENADA c);
+
+/**
+\brief Dá-nos a informação se a CASA da coordenada jogada é jogável (UM, DOIS, VAZIO)
+@param c Apontador para o estado
+@param c A coordenada
+@returns 1 se a jogada for possível; 0 senão
+int check_casa(ESTADO *e, COORDENADA c)
+*/
 int check_casa (ESTADO *e, COORDENADA c);
+
+/**
+\brief Verifica se a JOGADA é válida
+@param c Apontador para o estado
+@param c A coordenada
+@returns 1 se a jogada for possível; 0 senão
+int jogada_valida(ESTADO *e, COORDENADA c)
+*/
 int jogada_valida (ESTADO *e, COORDENADA c);
+/**
+\brief Realiza a jogada
+@param c Apontador para o estado
+@param c A coordenada
+@returns 0 se o jogo proseguir; 1 se o Jogador1 ganhar; 2 se o Jogador2 ganhar; 3 se a jogada for inválida
+int jogar(ESTADO *e, COORDENADA c)
+*/
 int jogar(ESTADO *e, COORDENADA c);
 /**
 \brief Devolve o vencedor do jogo
