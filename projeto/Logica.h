839714e68dfa8e4b9ee5c0aad28c9d73dd56957b
Carlos Alberto Fernandes Dias da Silva <62109586+bekogod@users.noreply.github.com>
2020-05-02 16:51:47 +0100

    Update Logica.h

--- projeto/Logica.h
+++ projeto/Logica.h
@@ -7,7 +7,6 @@
 \brief Dá-nos a informação se a coordenada jogada existe no tabuleiro
 @param c A coordenada
 @returns 1 se a jogada for possível; 0 senão
-int check_tabuleiro (COORDENADA c)
 */
 int check_tabuleiro (COORDENADA c);
 
@@ -16,7 +15,6 @@ int check_tabuleiro (COORDENADA c);
 @param c Apontador para o estado
 @param c A coordenada
 @returns 1 se a jogada for possível; 0 senão 
-int check_proximidade(ESTADO *e, COORDENADA c)
 */
 int check_proximidade (ESTADO *e, COORDENADA c);
 
@@ -25,7 +23,6 @@ int check_proximidade (ESTADO *e, COORDENADA c);
 @param c Apontador para o estado
 @param c A coordenada
 @returns 1 se a jogada for possível; 0 senão
-int check_casa(ESTADO *e, COORDENADA c)
 */
 int check_casa (ESTADO *e, COORDENADA c);
 
@@ -34,7 +31,6 @@ int check_casa (ESTADO *e, COORDENADA c);
 @param c Apontador para o estado
 @param c A coordenada
 @returns 1 se a jogada for possível; 0 senão
-int jogada_valida(ESTADO *e, COORDENADA c)
 */
 int jogada_valida (ESTADO *e, COORDENADA c);
 
@@ -43,7 +39,6 @@ int jogada_valida (ESTADO *e, COORDENADA c);
 @param c Apontador para o estado
 @param c A coordenada
 @returns 0 se o jogo prosseguir; 1 se o Jogador1 ganhar; 2 se o Jogador2 ganhar; 3 se a jogada for inválida
-int jogar(ESTADO *e, COORDENADA c)
 */
 int jogar(ESTADO *e, COORDENADA c);
 
@@ -52,7 +47,6 @@ int jogar(ESTADO *e, COORDENADA c);
 @param e Apontador para o estado
 @param c A coordenada
 @returns 1 se o jogador 1 for o vencedor; 2 se o jogador 2 for o vencedor; 0 se o jogo não tiver chegado ao fim
-int ganha_casa (ESTADO *e, COORDENADA c)
 */
 int ganha_casa (ESTADO *e, COORDENADA c);
 
@@ -61,7 +55,6 @@ int ganha_casa (ESTADO *e, COORDENADA c);
 @param e Apontador para o estado
 @param c A coordenada
 @returns 1 se o jogador 1 for o vencedor; 2 se o jogador 2 for o vencedor; 0 se o jogo não tiver chegado ao fim
-int ganha_invalido (ESTADO *e, COORDENADA c)
 */
 int ganha_invalido (ESTADO *e, COORDENADA c);
 
@@ -70,7 +63,6 @@ int ganha_invalido (ESTADO *e, COORDENADA c);
 @param e Apontador para o estado
 @param c A coordenada
 @returns 1 se o jogador 1 for o vencedor; 2 se o jogador 2 for o vencedor; 0 se o jogo não tiver chegado ao fim
-int vencedor (ESTADO *e, COORDENADA c)
 */
 int vencedor (ESTADO *e, COORDENADA c);
 
@@ -79,7 +71,6 @@ int vencedor (ESTADO *e, COORDENADA c);
 @param e Apontador para o estado
 @param c A coordenada
 @returns Apontador da lista criada
-LISTA lista_cand(ESTADO *e, COORDENADA c)
 */
 LISTA lista_cand(ESTADO *e, COORDENADA c);
 
@@ -87,7 +78,6 @@ LISTA lista_cand(ESTADO *e, COORDENADA c);
 \brief Devolve uma coordenada 
 @param e Apontador para o estado
 @returns Uma coordenada selecionada pela heurística da aleatoriedade 
-COORDENADA jog(ESTADO *e)
 */
 COORDENADA jog(ESTADO *e);
 
